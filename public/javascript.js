64adbf36a989c3747dccd5564581e9b3390d1982
jacklee137 <jack.lee137@outlook.com>
2023-08-01 15:58:45 +0300

    2134321

--- public/javascript.js
+++ public/javascript.js
@@ -1,6 +1,37 @@
 // Кнопка сообщение
 
 
+/* При клике показываем или скрываем кнопки мессенджеров */
+
+var menuBtn = $('.messenger-btn'),
+menu = $('.messenger-links');
+menuBtn.on('click', function() {
+if ( menu.hasClass('show') ) {
+menu.removeClass('show');
+} else {
+menu.addClass('show');
+}
+});
+
+/*  Скрыть div при клике в любом месте сайта кроме самого div */
+
+$(document).mouseup(function (e){
+var div = $('.messenger');
+if (!div.is(e.target)
+&& div.has(e.target).length === 0) {
+$('.messenger-links').removeClass('show');
+}
+});
+
+
+
+
+
+
+
+
+
+
 
 
 $(".pulse").on("click", function (event) {
@@ -180,3 +211,6 @@ $(window).scroll(function () {
   }
 
   getFaqAccordion();
+
+
+
